7599eb0487be8d2f0f9c52489f5c1a0d621a984f
codelikeachode <90079899+codelikeachode@users.noreply.github.com>
2024-09-22 15:56:44 -0400

    admin add

--- app.py
+++ app.py
@@ -3,3 +3,64 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://blog.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_published = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    articles = Article.query.order_by(Article.date_published.desc()).all()
+    return render_template('home.html', articles=articles)
+
+@app.route('/article/<int:id>'):
+def article(id):
+    article = Article.query.get_or_404(id)
+    return render_template('article.html', article=article)
+
+@app.route('/admin')
+@login_required
+def admin():
+    articles = Article.query.order_by(Article.date_published.desc()).all()
+    return render_template('admin/dashboard.html', articles=articles)
+
+@app.route('/admin/add', methods=['GET', 'POST'])
+@login_required
+def add_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_article = Article(title=title, content=content)
+        db.session.add(new_article)
+        db.session.commit()
+        flash('Article added successfully', 'success')
+        return redirect(url_for('admin'))
+    return render_template('admin/add_article.html')
